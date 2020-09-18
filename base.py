d9129c69739b7816bd4665b4c816c4ea78e57fcb
Scott Taylor <sgtaylor16@gmail.com>
2020-09-18 11:00:44 -0400

    Simulate appears to be working. Need to test.

--- base.py
+++ base.py
@@ -2,18 +2,23 @@ from dateutil.parser import parse
 import pandas as pd
 import re
 import datetime
+from seaborn import distplot
+from numpy import nan
 
 class task:
-    def __init__(self,id,task,dur,preds):
+    def __init__(self,id,task,dur,preds,scipy_object=None):
         '''initiator method
         Arguments:
         id: int, unique number for the task
         dur: float, number of days for the task
         preds: list of id's of predacessor tasks
+        Issues:
+        -predecessors must be given as a string, not a list.
         '''
         self.id = int(id)
         self.task = task
         self.duration = float(dur)
+        self.dist = scipy_object
         numbers = re.findall('[0-9]{1,}',preds)
         if len(numbers) > 0:
             self.predecessors = [int(i) for i in numbers]
@@ -29,6 +34,22 @@ class task:
         tempframe["Predecessors"] = self.predecessors
         return tempframe
 
+    def random_duration(self):
+        '''Method to return a random value from the tasks duration distrubution (if a scipy.stats object has 
+        been provided)'''
+        if self.dist is None:
+            return self.duration
+        else:
+            return self.dist.rvs(size = 1)[0]
+
+    def set_scipy(self,scipy_object):
+        '''Accessor method to set the dist attribute to a scipy.stats object'''
+        self.dist = scipy_object
+
+    def show_dist(self,size = 1000):
+        '''Method to give a graphical depiction of the task duration distribution'''
+        distplot(self.dist.rvs(size=size))
+        
 class project:
     def __init__(self,startdate = None):
         if startdate == None:
@@ -39,9 +60,11 @@ class project:
             raise ValueError
         self.taskdir = {}
         self.linksdf = pd.DataFrame(columns = ['EarlyStart','EarlyFinish','LateStart','LateFinish'])
+        self.taskdf = pd.DataFrame(columns = ['Task','Duration','Predecessors'])
+        self.durations = None
         
     def addTask(self,task_ob):
-        self.taskdir[task_ob.id] = task_ob
+        self.taskdir[task_ob.id] = task_ob 
         self.linksdf.loc[task_ob.id,'EarlyStart'] = self.startdate
         self.linksdf.loc[task_ob.id,'EarlyFinish'] = self.startdate
 
@@ -70,17 +93,25 @@ class project:
             self.taskdir[taskseries['TaskID']] = task(taskseries['TaskID'],taskseries['Task'],taskseries['Duration'],taskseries['Predecessors'])
             self.linksdf.loc[taskseries['TaskID'],'EarlyStart'] = self.startdate
             self.linksdf.loc[taskseries['TaskID'],'EarlyFinish'] = self.startdate + datetime.timedelta(days = int(taskseries['Duration']))
+            self.taskdf.loc[taskseries['TaskID'],:] = [taskseries['Task'],taskseries['Duration'],taskseries['Predecessors']]
+            self.durations = self.taskdf['Duration'].copy()
+        
+        #Set the start variable
+        self.findstart()
+
+            #Clean this up
     
-    def showTaskTable(self):
-        '''Method to show a table of the project's tasks'''
-        mycolumns = ['TaskID','Task','Duration','Predecessors']
-        outdf = pd.DataFrame(columns = mycolumns)
-        for task in self.taskdir.values():
-            columnlist = [task.id,task.task,task.duration,task.predecessors]
-            tempdf = pd.DataFrame(index = mycolumns, data = columnlist).T
-            outdf = pd.concat([outdf,tempdf],axis=0)
-        return outdf
-    
+    def initialize_linksdf(self):
+        self.linksdf = pd.DataFrame(columns = ['EarlyStart','EarlyFinish','LateStart','LateFinish'])
+        
+    def _reset_linksdf(self):
+        for row in self.linksdf.index:
+            self.linksdf.loc[row,'EarlyStart'] = self.startdate
+            durdays = int(self.durations[row])
+            self.linksdf.loc[row,'EarlyFinish'] = self.startdate + datetime.timedelta(days = durdays)
+        self.linksdf['LateStart'] = float('NaN')
+        self.linksdf['LateFinish'] = float('NaN')
+
     def findChildren(self,taskID):
         '''Method to find the direct children of a given task'''
         children = []
@@ -99,18 +130,17 @@ class project:
         '''Method to find the direct parents of a given task'''
         
         return self.taskdir[taskID].predecessors
-
-    def feedforward(self):
-        self.taskdir[self.startid].esd = self.startdate
-        self.taskdir[self.startid].efd = self.startdate + datetime.timedelta(days = self.taskdir[self.startid].duration)
         
-    def forwardprop(self,taskID):
+    def forwardprop(self,taskID = None):
         '''Method to run the forward propagation of the Gantt chart to determine the project length'''
         #Get the children
+        if taskID is None:
+            taskID = self.startid
+
         kids = self.findChildren(taskID)
         if len(kids) == 0: #If no children are found, then you are at completion. Kick out of the function.
-            self.linksdf.loc[:,'LateFinish'] = self.linksdf.loc[taskID,'EarlyFinish']
-            self.linksdf.loc[:,'LateStart'] = self.linksdf.loc[taskID,'EarlyStart']
+            self.linksdf.loc[:,'LateFinish'] = self.linksdf.loc[taskID,'EarlyFinish'] #Set the late start adn late finish 
+            self.linksdf.loc[:,'LateStart'] = self.linksdf.loc[taskID,'EarlyStart']   # In anticipation of calling backprop
             for parent in self.findParents(taskID):
                 self.backwardprop(taskID)
         else:
@@ -118,7 +148,8 @@ class project:
                tempesd = self.linksdf.loc[taskID,'EarlyFinish']
                if tempesd > self.linksdf.loc[child,'EarlyStart']:
                    self.linksdf.loc[child,'EarlyStart'] = tempesd  #Shift the start date
-                   self.linksdf.loc[child,'EarlyFinish'] = tempesd + datetime.timedelta(days = self.taskdir[child].duration)
+                   durdays = int(self.durations[child])
+                   self.linksdf.loc[child,'EarlyFinish'] = tempesd + datetime.timedelta(days =durdays)
                self.forwardprop(child)
 
     def backwardprop(self,taskID):
@@ -134,6 +165,31 @@ class project:
                 templfd = self.linksdf.loc[taskID,'LateStart']
                 if templfd <= self.linksdf.loc[parent,'LateFinish']:
                     self.linksdf.loc[parent,'LateFinish'] = templfd # Shift the finish date
-                    self.linksdf.loc[parent,'LateStart'] = templfd - datetime.timedelta(days = self.taskdir[parent].duration)
+                    durdays = int(self.durations[parent])
+                    self.linksdf.loc[parent,'LateStart'] = templfd - datetime.timedelta(days = durdays)
                 self.backwardprop(parent)
 
+    def add_dist(self,taskID,scipy_object):
+        self.taskdir[taskID].set_scipy(scipy_object)
+
+    def sample(self):
+        for task in self.taskdir.values():
+            self.durations[task.id] = task.random_duration()
+
+    def summarytable(self):
+        return pd.merge(self.taskdf,self.linksdf,left_index = True, right_index = True)
+
+
+def simulate(project,nsamp = 10):
+    '''Function to simulate a project to create a distribution
+    Arguments:
+    project, base.project class: project class to simulate
+    nsamp, int: Number of simulations to run
+    '''
+    results = []
+    for i in range(nsamp):
+        project.sample()  #Populate the distributions attribute with random variables
+        project.forwardprop(project.startid) #Run the distributions
+        results.append(project.linksdf["EarlyFinish"].max())
+        project._reset_linksdf()
+    return results
